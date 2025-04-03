c6f1ebb9e0b73c8737b6367aee126733cf22b7fb
keeee21 <keisuke.u211016@gmail.com>
2025-04-03 15:32:03 +0900

    feat: 本番環境でログを出力しない

--- vue-test/src/main.ts
+++ vue-test/src/main.ts
@@ -8,6 +8,24 @@ import router from './lib/router';
 
 const app = createApp(App);
 
+// 本番環境でログは出力しない
+if (import.meta.env.VITE_NODE_ENV === 'production') {
+  const no = () => {
+    // 何もしない
+  };
+  console.log = no;
+  console.warn = no;
+  console.debug = no;
+  console.info = no;
+  console.error = no;
+  console.table = no;
+  console.time = no;
+  console.timeEnd = no;
+  console.group = no;
+  console.groupEnd = no;
+  console.trace = no;
+}
+
 app.use(createPinia());
 app.use(router);
 
